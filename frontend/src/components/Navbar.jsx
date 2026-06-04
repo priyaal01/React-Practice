a86742ff3da513933b3a6b8a68edff57c54bc675
Priyaal Gayakwad <priyaalgayakwad3@gmail.com>
2026-06-04 14:18:09 +0530

    getting some basics done

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='nav'>
+        <div>
+          <h1>Navbar</h1>
+        </div>
+        <div className='nav2'>
+          <ul className='nav3'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Profile</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+   
+  )
+}
+
+export default Navbar
