cccecea3aa0c35032d6d70b1990e78c4cc5c511b
Priyaal Gayakwad <priyaalgayakwad3@gmail.com>
2026-06-07 16:41:10 +0530

    understanding hooks properly

--- frontend/src/components/Section2/Section2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Section2 = () => {
-  return (
-    <div className='bg-amber-300 h-screen w-full col text-amber-50'>Section2</div>
-  )
-}
-
-export default Section2
---
+++ frontend02/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>Counter:{count}</h2>
+      <button onClick={() => setCount(count + 1)}>increase</button>
+      <button onClick={() => setCount(count - 1)}>decrease</button>
+      <button onClick={() => setCount(count + 5)}>Jump by 5</button>
+      <button onClick={() => setCount(0)}>reset</button>
+    </div>
+    
+  )
+}
+
+export default App
