5fca4fd0b25bfae81c683c79703c4d7dca07e7c8
Priyaal Gayakwad <priyaalgayakwad3@gmail.com>
2026-06-10 23:54:36 +0530

    edited

--- frontend02/src/App.jsx
+++ frontend02/src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
       <button onClick={() => setCount(count + 1)}>increase</button>
       <button onClick={() => setCount(count - 1)}>decrease</button>
       <button onClick={() => setCount(count + 5)}>Jump by 5</button>
+      <button onClick={()=>  setCount(count+2)}> jump by 2</button>
       <button onClick={() => setCount(0)}>reset</button>
     </div>
     
